8a0fd100f47ce9bf1589f05118fbe3fae22f614c
August Posner <august_posner@hotmail.com>
2018-02-05 16:39:13 +0100

    added movement

---
+++ Break Out/Scripts/Ball.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+func _ready():
+	
+	pass
+
+func _process(delta):
+	var bodies = get_colliding_bodies()
+	
+	for body in bodies:
+		if body.is_in_group("Bricks"):
+			body.queue_free()
+	
+	pass
--- Break Out/Scripts/Paddle.gd
+++ Break Out/Scripts/Paddle.gd
@@ -1,12 +1,12 @@
 extends KinematicBody2D
 
-
 func _ready():
 	
 	pass
 
 func _process(delta):
+	var maxX = get_viewport().size.x
 	var y = get_position().y
-	var x = get_viewport().get_mouse_position().x
+	var x = min(max(0,get_viewport().get_mouse_position().x), maxX)
 	set_position(Vector2(x,y))
 	pass
